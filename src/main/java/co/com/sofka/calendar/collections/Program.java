f1294f25db0c79d4db96b7719316ef4cbdf67e3f
Johan Ramirez <johan911019@gmail.com>
2021-12-10 16:48:54 -0500

    Se crea el controller

--- src/main/java/co/com/sofka/calendar/collections/Program.java
+++ src/main/java/co/com/sofka/calendar/collections/Program.java
@@ -2,7 +2,6 @@ package co.com.sofka.calendar.collections;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.util.List;
 
 @Document
---
+++ src/main/java/co/com/sofka/calendar/repositories/ProgramDateRepository.java
@@ -0,0 +1,6 @@
+package co.com.sofka.calendar.repositories;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface ProgramDateRepository extends ReactiveMongoRepository {
+}
